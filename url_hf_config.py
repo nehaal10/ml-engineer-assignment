08984fe11eef9efd7d2d5d3e724d24f7eea04cf2
nehaal <72294250+nehaal10@users.noreply.github.com>
2023-05-30 18:51:35 +0530

    config file

---
+++ url_hf_config.py
@@ -0,0 +1,12 @@
+'''config file which contails all urls and hf_pipeline names'''
+
+
+class Config:
+    task_urls = {
+        "object-detection": "https://test-object-detect-nikhil-ws.tfy-ctl-euwe1-devtest.devtest.truefoundry.tech/v2/models/test-object-detect/infer",
+        "text-generation": "https://text-generation-ml-intern-assign.tfy-gcp-standard-usce1.devtest.truefoundry.tech/v2/models/text-generation/infer",
+        "token-classification": "https://token-class-ml-intern-assign.tfy-gcp-standard-usce1.devtest.truefoundry.tech/v2/models/token-class/infer",
+        "zero-shot-classification": "https://zero-shot-class-ml-intern-assign.tfy-gcp-standard-usce1.devtest.truefoundry.tech/v2/models/zero-shot-class/infer"
+    }
+
+    custom_deployed_url = 'https://ml-assignment-workspace-8000.demo1.truefoundry.com/predict'
